92170f53c5289db1e71fa1a6e4058caf65f17d13
ddowoogi <donguk348@gmail.com>
2022-10-11 22:22:40 +0900

    Codeup

--- algorithm/Codeup1018.java
+++ algorithm/Codeup1018.java
@@ -1,7 +1,7 @@
 package algorithm;
 
 import java.util.Scanner;
-public class Coudeup1018 {
+public class Codeup1018 {
     public static void main(String args[]) { 
         
         Scanner sc = new Scanner(System.in);
