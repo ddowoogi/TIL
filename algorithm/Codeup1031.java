66511f53828fd407cfe52c94f91930a30ce48df4
ddowoogi <donguk348@gmail.com>
2022-10-20 20:44:24 +0900

    Codeup

---
+++ algorithm/Codeup1031.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+import java.util.Scanner;
+public class Codeup1031 {
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        int data=sc.nextInt();
+
+        System.out.printf("%o",data);
+
+        sc.close();
+
+    }
+    
+}
