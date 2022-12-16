904a2588c815ba2bc32e6a69338ba6e6b0dd3ecd
ddowoogi <donguk348@gmail.com>
2022-12-16 21:39:18 +0900

    Codeup1074

---
+++ algorithm/Codeup1074.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Codeup1074 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int i=sc.nextInt();
+
+        while(i>0){
+            System.out.println(i);
+            i--;
+        }
+        sc.close();
+    }
+}
