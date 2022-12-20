cb1377e8790ee02c8cb9972079c6f144d6150736
ddowoogi <donguk348@gmail.com>
2022-12-20 22:31:18 +0900

    Codeup1079

---
+++ algorithm/Codeup1079.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Codeup1079 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+       
+
+       while(true){
+        char a=sc.next().charAt(0);
+
+        if(a=='q'){
+            System.out.println(a);
+            break;
+        }
+
+       }
+       sc.close();
+    }
+}
