705d154a54148fc019b92e0498a3f73acf8b0cc3
ddowoogi <donguk348@gmail.com>
2022-12-28 22:38:56 +0900

    Codeup1088

---
+++ algorithm/Codeup1088.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Codeup1088 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			if(i % 3 != 0) {
+			System.out.print(i+" ");
+			}
+		}
+        sc.close();
+	}
+
+}
