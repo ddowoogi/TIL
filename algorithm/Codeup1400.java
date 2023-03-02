6dadaf9db0d2590f174d164b0fa0f92e86d13bc6
ddowoogi <donguk348@gmail.com>
2023-03-02 20:11:59 +0900

    Codeup1400

---
+++ algorithm/Codeup1400.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+
+import java.util.Scanner;
+public class Codeup1400{
+    public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+
+     String a=sc.nextLine();
+
+     if(a=="love"){
+        System.out.print("I love you");
+     }
+      
+     sc.close();
+    }
+}
