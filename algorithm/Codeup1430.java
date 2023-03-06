d1c17cd9f2c19cda2d74c06fd17ff9de04b299bf
ddowoogi <donguk348@gmail.com>
2023-03-06 21:06:40 +0900

    Codeup1430

---
+++ algorithm/Codeup1430.java
@@ -0,0 +1,56 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Codeup1430 {   
+
+//  5
+//	2 52 23 55 100
+//	4
+//	5 2 55 99
+
+	public static void main(String[] args) {
+
+	//값을 입력받음
+		Scanner sc = new Scanner(System.in);
+		
+	
+		
+		int k = sc.nextInt();
+		int [] arr = new int[k];
+		
+		for(int i = 0; i < k; i++){
+			arr[i] = sc.nextInt();
+	}
+	
+	
+		int t = sc.nextInt();
+		int [] arr2 = new int[t];
+		
+		for(int i = 0; i < t; i++){
+			arr2[i] = sc.nextInt();
+	}
+	
+		
+		int [] out = new int[arr2.length];
+		
+		for(int i = 0; i < arr2.length; i++) {
+			for(int j = 0 ; j <arr.length; j++) {
+				if(arr2[i] == arr[j]) {
+				out[i] = 1;
+			} 
+			}
+				
+			
+		}
+		
+				
+		for(int i = 0; i < out.length; i++) { // 결과출력용
+			System.out.printf("%d ",out[i]);
+			
+		} 
+        sc.close();
+		
+ }
+		
+}
